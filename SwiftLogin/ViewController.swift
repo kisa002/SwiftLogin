f01f75ca849addeae72e89eecf32be7594b38564
HolyKnight <gmyou@pentasecurity.com>
2020-07-16 18:51:11 +0900

    Initial Commit

---
+++ SwiftLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLogin
+//
+//  Created by HolyKnight on 2020/07/16.
+//  Copyright © 2020 HolyKnight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftLoginTests/SwiftLoginTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftLoginTests.swift
+//  SwiftLoginTests
+//
+//  Created by HolyKnight on 2020/07/16.
+//  Copyright © 2020 HolyKnight. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftLogin
+
+class SwiftLoginTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
